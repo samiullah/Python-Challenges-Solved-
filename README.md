06ad42de44de1b528d5d5c05f523d8bd00ac3310
SAMI <sami@Apples-MacBook-Pro.local>
2017-11-27 01:35:09 +0530

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+1. Write a program to change url parameter
+2. Write a program to reverse first and last name 
+3. Write a program to print file extension
+
---
+++ colorpicker.py
@@ -0,0 +1,7 @@
+# Write a Python program to display
+# the first and last colors from
+# the following list. 
+color_list = ["Red","Green","White" ,"Black"]
+print(color_list[0] + ' and ' + color_list[-1] )
+
+
