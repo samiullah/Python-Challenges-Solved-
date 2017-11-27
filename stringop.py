2c69f9cadf229ad932415427abd496b7f96902a2
SAMI <sami@Apples-MacBook-Pro.local>
2017-11-27 10:52:27 +0530

    Day2-Add string operations python program

---
+++ stringop.py
@@ -0,0 +1,10 @@
+# Python program that 
+# accepts an integer (n) and 
+# computes the value of n+nn+nnn. 
+# Sample value of n is 5 
+# Expected Result : 615
+n=input("Please enter the number")
+n1=int(n)
+n2=int(2*n)
+n3=int(3*n)
+print(n1+n2+n3)
